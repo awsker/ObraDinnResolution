1d21a72906f884996e4ad9b33efa3c244cdf8544
unknown <asker@sealhunter.com>
2023-12-20 16:54:48 +0100

    Initial commit

---
+++ MonitorPatch.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+
+namespace ResolutionFix
+{
+    [HarmonyPatch]
+    internal class MonitorPatch
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(Monitor), "OnEnable")]
+        public static void Postfix_OnEnable(Monitor __instance)
+        {
+            __instance.blurAtEdges = false;
+            __instance.borderTexture = null;
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,59 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ResolutionFix
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"   Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+
+            SceneManager.sceneLoaded += sceneManager_sceneLoaded;
+            //SceneManager.activeSceneChanged += sceneManager_activeSceneChanged;
+        }
+
+        private void sceneManager_sceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            Logger.LogInfo($"   SCENE LOADED: " + scene.name);
+            fixScene();
+        }
+
+        private void fixScene()
+        {
+            var scene = SceneManager.GetActiveScene();
+            
+            foreach (var root in scene.GetRootGameObjects())
+            {
+                /*
+                var monitor = root.GetComponentInChildren<Monitor>();
+                if (monitor != null)
+                {
+                    monitor.blurAtEdges = false;
+                    monitor.borderTexture = null;
+                }
+                var onebit = root.GetComponentInChildren<OneBit>();
+                if (onebit != null)
+                {
+                    onebit.linedSettings.distortFringe = false;
+                }*/
+                var meshRenderers = root.GetComponentsInChildren<MeshRenderer>();
+                foreach (var mesh in meshRenderers)
+                {
+                    if (mesh.gameObject.name.Contains("blur") || mesh.gameObject.name.Contains("Blur"))
+                    {
+                        mesh.gameObject.SetActive(false);
+                    }
+                }
+            }
+            Settings.outputMode = Settings.OutputMode.Digital0;
+        }
+    }
+}
