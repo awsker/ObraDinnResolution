e2afb7af62f8bcf0cfff71c94855bf1b216bc228
Tom <tomwashere@gmail.com>
2024-03-08 20:07:21 +1300

    Create DialogPatch.cs

---
+++ DialogPatch.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+
+namespace ResolutionFix
+{
+    [HarmonyPatch]
+    public class DialogPatch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(Dialog), "Play")]
+        public static void Prefix_Play()
+        {
+            Plugin.IsHighRes = false;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(Dialog), "Stop")]
+        public static void Prefix_Stop()
+        {
+            Plugin.IsHighRes = true;
+        }
+    }
+}
